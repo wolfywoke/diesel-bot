f36743309a91df65742140c73499a0d81411c8db
wolfywoke <wolfywoke@gmail.com>
2026-02-22 22:32:39 +0100

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,23 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
 console.log("Diesel bot starting...");
+
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+  console.log("Scan this QR code:");
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log("WhatsApp bot is ready.");
+});
+
+client.on('message', message => {
+  console.log("Message received:", message.body);
+});
+
+client.initialize();

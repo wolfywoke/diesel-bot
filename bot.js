0bbe347f18a1110e3dd7f40dc02ae4519977c4f8
wolfywoke <wolfywoke@gmail.com>
2026-02-22 23:31:17 +0100

    bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,26 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
+const QRCode = require('qrcode');
 
 console.log("Diesel bot starting...");
 
-const QRCode = require('qrcode');
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu'
+    ]
+  }
+});
 
 client.on('qr', async (qr) => {
   console.log("Generating QR image...");
-
   const url = await QRCode.toDataURL(qr);
-
   console.log("OPEN THIS IN YOUR BROWSER:");
   console.log(url);
-});
-
-client.on('qr', (qr) => {
-  console.log("QR STRING BELOW:");
-  console.log(qr);
 });
 
 client.on('ready', () => {
